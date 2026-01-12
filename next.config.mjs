d8d437b877f47f14f20d08946e096694b4866aaf
Santiago Correa <109907690+algorithmicsanti@users.noreply.github.com>
2026-01-12 15:47:06 -0600

    Restore component and use simple config

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
     // Ensure strict match for all internal links
     trailingSlash: false,
-    // Use standalone output for efficient lambda bundling
-    output: "standalone",
 };
 
 export default nextConfig;
